e4df5535391f6727358ea266c0295153e195ef83
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2025-12-07 19:49:27 +0500

    frontend authentication work register, login, complete and work for otp veryfication

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import { useContext, useState } from "react";
 import "../styles/Auth.css";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const Auth = () => {
-  return <> </>;
+  const { isAuthenticated } = useContext(AuthContext);
+  const [isLogin, setIsLogin] = useState(true);
+  if (isAuthenticated) {
+    return <Navigate to="/" />
+  }
+  return <>
+    <div className="auth-page">
+      <div className="auth-container">
+        <div className="auth-toggle">
+          <button
+            className={`toggle-btn ${isLogin ? "active" : ""}`}
+            onClick={() => setIsLogin(true)}
+          >
+            Login
+          </button>
+          <button
+            className={`toggle-btn ${!isLogin ? "active" : ""}`}
+            onClick={() => setIsLogin(false)}
+          >
+            Register
+          </button>
+        </div>
+        {isLogin ? <Login /> : <Register />}
+      </div>
+    </div>
+  </>;
 };
 
 export default Auth;
